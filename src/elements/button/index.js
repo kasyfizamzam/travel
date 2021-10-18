700b681991e61e9cea83090008a14e9d9e89f2e0
kasyfizamzam <kasyfi.zamzam@gmail.com>
2021-10-18 14:59:09 +0800

    add some element

--- src/elements/button/index.js
+++ src/elements/button/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import propTypes from 'prop-types'
 
-export default function button([prop]) {
+export default function button([props]) {
+    const className = [props.className]
+    if (props.isPrimary) className.push("btn-primary")
+    if (props.isSmall) className.push("btn-sm")
+    if (props.isLarge) className.push("btn-lg")
+    if (props.isBlock) className.push("btn-block")
+    if (props.hasShadow) className.push("btn-shadow")
+
+    const onClick = () => {
+        if(props.onClick) props.onClick()
+    }
+
     return (
         <div>
-            
         </div>
     )
 }
+
+button.propTypes = {
+    type: propTypes.oneOf(["button", "link"]),
+    onClick: propTypes.func,
+    target: propTypes.string,
+    className: propTypes.string,
+    isDisabled: propTypes.bool,
+    isLoading: propTypes.bool,
+    isSmall: propTypes.bool,
+    isLarge: propTypes.bool,
+    isBlock: propTypes.bool,
+    hasShadow: propTypes.bool,
+};
